9e955f2e0073188ce0009ff9aaee8b5b2b87652e
Marco Milanese <marcomilanese7@gmail.com>
2017-03-13 15:53:33 +0100

    Add `cs_group_name`

--- src/capstone.rs
+++ src/capstone.rs
@@ -331,4 +331,32 @@ impl Capstone {
             Err(_) => None,
         }
     }
+
+    /// Return friendly name of group.
+    ///
+    /// Returns `None` if `group_id` is invalid. You can find the group mapping in Capstone's
+    /// C headers (e.g. x86.h for x86).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use capstone_rust::capstone as cs;
+    ///
+    /// let dec = cs::Capstone::new(cs::cs_arch::CS_ARCH_X86, cs::cs_mode::CS_MODE_32).unwrap();
+    /// assert_eq!(dec.group_name(2).unwrap(), "call");
+    /// ```
+    pub fn group_name(&self, group_id: u32) -> Option<&str> {
+        let name = unsafe {
+            let name = cs_group_name(self.handle.get(), group_id);
+            if name == 0 as *const i8 {
+                return None;
+            }
+            CStr::from_ptr(name)
+        };
+
+        match name.to_str() {
+            Ok(s)  => Some(s),
+            Err(_) => None,
+        }
+    }
 }
