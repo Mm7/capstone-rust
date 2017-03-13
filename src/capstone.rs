a3676e11a9b8bf96ba3ec3cdda45b473b7bd2433
Marco Milanese <marcomilanese7@gmail.com>
2017-03-13 22:24:41 +0100

    Initial instruction details support

--- src/capstone.rs
+++ src/capstone.rs
@@ -6,6 +6,7 @@ use std::cell::Cell;
 use std::error::Error;
 use std::ffi::CStr;
 use std::fmt;
+use std::mem;
 
 /// Get the version of the capstone engine.
 ///
@@ -106,12 +107,15 @@ pub struct Instr {
     pub bytes: Vec<u8>,
     pub mnemonic: String,
     pub op_str: String,
-    pub detail: Option<()>,
+    pub detail: Option<Details>,
 }
 
 impl Instr {
     /// Create an `Instr` from a cs_insn.
-    pub fn new(instr: &cs_insn) -> Instr {
+    pub fn new(instr: &cs_insn, decode_detail: bool, arch: cs_arch) -> Instr {
+        assert_ne!(arch, cs_arch::CS_ARCH_MAX);
+        assert_ne!(arch, cs_arch::CS_ARCH_ALL);
+
         let mut bytes = Vec::new();
         for i in 0..instr.bytes.len() {
             bytes.push(instr.bytes[i]);
@@ -133,16 +137,104 @@ impl Instr {
             op_str.push((instr.op_str[i] as u8) as char);
         }
 
-        Instr{
+        let detail = if decode_detail {
+            let detail = unsafe {*instr.detail};
+            let arch_union = detail.__bindgen_anon_1;
+
+            let arch = unsafe { match arch {
+                cs_arch::CS_ARCH_ARM   => { DetailsArch::ARM  (*arch_union.arm.as_ref())   },
+                cs_arch::CS_ARCH_ARM64 => { DetailsArch::ARM64(*arch_union.arm64.as_ref()) },
+                cs_arch::CS_ARCH_MIPS  => { DetailsArch::MIPS (*arch_union.mips.as_ref())  },
+                cs_arch::CS_ARCH_X86   => { DetailsArch::X86  (*arch_union.x86.as_ref())   },
+                cs_arch::CS_ARCH_PPC   => { DetailsArch::PPC  (*arch_union.ppc.as_ref())   },
+                cs_arch::CS_ARCH_SPARC => { DetailsArch::SPARC(*arch_union.sparc.as_ref()) },
+                cs_arch::CS_ARCH_SYSZ  => { DetailsArch::SYSZ (*arch_union.sysz.as_ref())  },
+                cs_arch::CS_ARCH_XCORE => { DetailsArch::XCORE(*arch_union.xcore.as_ref()) },
+                _ => panic!("Unexpected arch: {:?}", arch),
+            }};
+
+            let mut regs_read = Vec::new();
+            for i in 0..detail.regs_read_count {
+                regs_read.push(detail.regs_read[i as usize]);
+            }
+
+            let mut regs_write = Vec::new();
+            for i in 0..detail.regs_write_count {
+                regs_write.push(detail.regs_write[i as usize]);
+            }
+
+            let mut groups = Vec::new();
+            for i in 0..detail.groups_count {
+                groups.push(detail.groups[i as usize]);
+            }
+
+            Some(Details {
+                regs_read: regs_read,
+                regs_write: regs_write,
+                groups: groups,
+                arch: arch,
+            })
+        } else {
+            None
+        };
+
+        Instr {
             id: instr.id,
             address: instr.address,
             size: instr.size,
             bytes: bytes,
             mnemonic: mnemonic,
             op_str: op_str,
-            detail: None
+            detail: detail
         }
     }
+}
+
+/// Details of an instruction.
+///
+/// If CS_OPT_DETAIL is on, `Instr.detail` will be filled with this struct, that you can use to get
+/// some details of an instruction (e.g. registers read, modified, ...).
+///
+/// # Examples
+///
+/// ```
+/// use capstone_rust::capstone as cs;
+/// let code = vec![0x55, 0x48, 0x8b, 0x05, 0xb8, 0x13, 0x00, 0x00];
+///
+/// let dec = cs::Capstone::new(cs::cs_arch::CS_ARCH_X86, cs::cs_mode::CS_MODE_32).unwrap();
+/// dec.option(cs::cs_opt_type::CS_OPT_DETAIL, cs::cs_opt_value::CS_OPT_ON);
+///
+/// let buf = dec.disasm(code, 0, 0).unwrap();
+/// for x in buf.iter() {
+///     println!("{:x}: {} {}", x.address, x.mnemonic, x.op_str);
+/// }
+/// ```
+#[derive(Debug)]
+pub struct Details {
+    /// List of implicit registers read by this insn
+    pub regs_read: Vec<u8>,
+
+    /// List of implicit registers modified by this insn
+    pub regs_write: Vec<u8>,
+
+    /// List of group this instruction belong to
+    pub groups: Vec<u8>,
+
+    /// Architecture-specific details.
+    pub arch: DetailsArch,
+}
+
+/// Architecture-specific part of detail.
+#[derive(Debug)]
+pub enum DetailsArch {
+    X86(cs_x86),
+    ARM64(cs_arm64),
+    ARM(cs_arm),
+    MIPS(cs_mips),
+    PPC(cs_ppc),
+    SPARC(cs_sparc),
+    SYSZ(cs_sysz),
+    XCORE(cs_xcore),
 }
 
 /// Buffer of disassembled instructions.
@@ -151,6 +243,8 @@ impl Instr {
 pub struct InstrBuf {
     ptr: *mut cs_insn,
     count: usize,
+    decode_detail: bool,
+    arch: cs_arch,
 }
 
 impl Drop for InstrBuf {
@@ -161,9 +255,11 @@ impl Drop for InstrBuf {
 
 impl InstrBuf {
     /// Create an `InstrBuf` from a pointer to a cs_insn buffer. `count` is the number of
-    /// instructions in `insn`.
-    pub fn new(insn: *mut cs_insn, count: usize) -> InstrBuf {
-        InstrBuf{ptr: insn, count: count}
+    /// instructions in `insn`. `decode_detail` states if details are available for the
+    /// instructions in `insn`, if true `Instr` created by `get` will have `Details`. `arch` is the
+    /// architecture to use to interpret the arch-specific part of cs_detail.
+    pub fn new(insn: *mut cs_insn, count: usize, decode_detail: bool, arch: cs_arch) -> InstrBuf {
+        InstrBuf{ptr: insn, count: count, decode_detail: decode_detail, arch: arch}
     }
 
     /// Get the number of instructions in this buffer.
@@ -177,7 +273,7 @@ impl InstrBuf {
         let insn;
 
         unsafe { insn = &(*(self.ptr.offset(index as isize))) }
-        Instr::new(insn)
+        Instr::new(insn, self.decode_detail, self.arch)
     }
 
     /// Create an iterator from the beginning of this buffer.
@@ -220,6 +316,8 @@ impl<'a> InstrIter<'a> {
 /// Capstone handle.
 pub struct Capstone {
     handle: Cell<csh>,
+    details_on: Cell<bool>,
+    arch: cs_arch,
 }
 
 impl Drop for Capstone {
@@ -245,17 +343,35 @@ impl Capstone {
         unsafe { err = cs_open(arch, mode, &mut handle) };
         to_res(err)?;
 
-        Ok(Capstone{handle: Cell::new(handle)})
+        Ok(Capstone{handle: Cell::new(handle), details_on: Cell::new(false), arch: arch})
     }
 
     /// Set option for disassembling engine at runtime.
     ///
     /// `typ` type of option to set. `value` value of the option.
-    pub fn option(&self, typ: cs_opt_type, value: usize) -> Result<(), CsErr> {
+    pub fn option(&self, typ: cs_opt_type, value: cs_opt_value) -> Result<(), CsErr> {
         let err;
 
-        unsafe { err = cs_option(self.handle.get(), typ, value); };
-        to_res(err)
+        unsafe {
+            let value = mem::transmute::<cs_opt_value, u32>(value) as usize;
+            err = cs_option(self.handle.get(), typ, value);
+        };
+        to_res(err)?;
+
+        // When an instruction is decoded we need to know if details are generated (to decide if
+        // the `Details` struct must be created). Unfortunately Capstone doesn't provide a way to
+        // get the value of an option, so we are forced to track the `DETAIL` option from here.
+        if typ == cs_opt_type::CS_OPT_DETAIL {
+            self.details_on.set({
+                if value == cs_opt_value::CS_OPT_ON {
+                    true
+                } else {
+                    false
+                }
+            });
+        }
+
+        Ok(())
     }
 
     /// Disassemble binary code, given the code buffer, address and number of instructions to be
@@ -301,7 +417,7 @@ impl Capstone {
             return Err(CsErr::new(err));
         }
 
-        Ok(InstrBuf::new(insn, res))
+        Ok(InstrBuf::new(insn, res, self.details_on.get(), self.arch))
     }
 
     /// Return friendly name of register in a string.
