ae164e216c3968ad204b2654a105d7197bbe1345
Marco Milanese <marcomilanese7@gmail.com>
2017-03-13 22:23:08 +0100

    Implement Debug, Clone and Copy for `cs_arm`
    It seems that bindgen fails to derive these traits automatically.
    Implement them manually.

--- src/capstone_sys.rs
+++ src/capstone_sys.rs
@@ -3,4 +3,31 @@
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
 
+use std::fmt;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+// It seems that bindgen fails to derive Debug, Clone and Copy for `cs_arm`. Let's implement them
+// traits manually.
+impl fmt::Debug for cs_arm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut operands = String::new();
+        for i in self.operands.iter() {
+            operands.push_str(format!(" {:?}", i).as_str());
+        }
+
+        write!(f, "cs_arm {{ usermode: {:?}, vector_size: {:?}, vector_data: {:?}, \
+            cps_mode: {:?}, cps_flag: {:?}, cc: {:?}, update_flags: {:?}, writeback: {:?}, \
+            mem_barrier: {:?}, op_count: {:?},{}}}", self.usermode, self.vector_size,
+            self.vector_data, self.cps_mode, self.cps_flag, self.cc, self.update_flags,
+            self.writeback, self.mem_barrier, self.op_count, operands)
+    }
+}
+
+impl Clone for cs_arm {
+    fn clone(&self) -> cs_arm {
+        *self
+    }
+}
+
+impl Copy for cs_arm { }
